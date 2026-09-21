f78e62974fba9a57e8656a1ff429992ae4070787
MagicMerge Demo <demo@composal.ai>
2026-09-21 09:06:57 -0700

    06-ruby-replay-stub: theirs

--- vex-api/test/services/vcs/merge_queue/batch_change_set_test.rb
+++ vex-api/test/services/vcs/merge_queue/batch_change_set_test.rb
@@ -1,3 +1,4 @@
 replay = Object.new
 replay.define_singleton_method(:commit_for) { |change:, moving_tip:| moving_tip }
 replay.define_singleton_method(:flush!) { true }
+replay.define_singleton_method(:reader) { FakeJjStore.new }
