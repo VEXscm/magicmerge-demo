7b01192d75566e2cce4356c615c3ba6ea6120d59
MagicMerge Demo <demo@composal.ai>
2026-09-21 09:06:58 -0700

    08-git-view-delete-modify: ours

--- crates/vex-git-view/src/control.rs
+++ crates/vex-git-view/src/control.rs
@@ -1,8 +1,14 @@
 pub struct ProjectionControl {
     cancelled: bool,
+    storage: Option<String>,
 }
 
 impl ProjectionControl {
+    pub fn with_storage(mut self, storage: String) -> Self {
+        self.storage = Some(storage);
+        self
+    }
+
     pub fn check(&self) -> Result<(), ()> {
         if self.cancelled { Err(()) } else { Ok(()) }
     }
