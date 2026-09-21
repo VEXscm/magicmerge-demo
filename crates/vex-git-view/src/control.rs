c9acae2b65279eba4937f695a12dafb3bd40649a
MagicMerge Demo <demo@composal.ai>
2026-09-21 09:06:58 -0700

    08-git-view-delete-modify: theirs (delete)

--- crates/vex-git-view/src/control.rs
+++
@@ -1,9 +0,0 @@
-pub struct ProjectionControl {
-    cancelled: bool,
-}
-
-impl ProjectionControl {
-    pub fn check(&self) -> Result<(), ()> {
-        if self.cancelled { Err(()) } else { Ok(()) }
-    }
-}
